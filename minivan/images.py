e10886712af21a7688b656e9289920a1a3240389
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2025-02-04 12:38:32 -0800

    extract tracking groundtruth

---
+++ minivan/images.py
@@ -0,0 +1,162 @@
+from typing import TypeGuard, NewType
+
+import cv2
+import torch
+
+
+ImgCHW = NewType('ImgCHW', torch.Tensor)
+ImgHWC = NewType('ImgHWC', torch.Tensor)
+
+ImgNCHW = NewType('ImgNCHW', torch.Tensor)
+ImgNHWC = NewType('ImgNHWC', torch.Tensor)
+
+ImgGCHW = NewType('ImgGCHW', torch.Tensor)
+ImgGHWC = NewType('ImgGHWC', torch.Tensor)
+
+
+def isCHW(img: torch.Tensor) -> TypeGuard[ImgCHW]:
+    return img.dim() == 3 and img.shape[0] in {1, 3}
+
+def isHWC(img: torch.Tensor) -> TypeGuard[ImgHWC]:
+    return img.dim() == 3 and img.shape[2] in {1, 3}
+
+def isNCHW(img: torch.Tensor) -> TypeGuard[ImgNCHW]:
+    return img.dim() == 4 and img.shape[1] in {1, 3}
+
+def isNHWC(img: torch.Tensor) -> TypeGuard[ImgNHWC]:
+    return img.dim() == 4 and img.shape[3] in {1, 3}
+
+def isGCHW(img: torch.Tensor) -> TypeGuard[ImgGCHW]:
+    return img.dim() == 5 and img.shape[2] in {1, 3}
+
+def isGHWC(img: torch.Tensor) -> TypeGuard[ImgGHWC]:
+    return img.dim() == 5 and img.shape[4] in {1, 3}
+
+def padCHW(img: ImgCHW, h: int, w: int) -> ImgCHW:
+    C, H, W = img.shape
+    pad_h = (h - H % h) % h
+    pad_w = (w - W % w) % w
+    ret = torch.nn.functional.pad(img, (0, pad_w, 0, pad_h))
+    assert isCHW(ret), ret.shape
+    return ret
+
+def padHWC(img: ImgHWC, h: int, w: int) -> ImgHWC:
+    H, W, C = img.shape
+    pad_h = (h - H % h) % h
+    pad_w = (w - W % w) % w
+    ret = torch.nn.functional.pad(img, (0, 0, 0, pad_w, 0, pad_h))
+    assert isHWC(ret), ret.shape
+    return ret
+
+def splitCHW(img: ImgCHW, h: int, w: int) -> ImgGCHW:
+    assert img.shape[1] % h == 0
+    assert img.shape[2] % w == 0
+    ret = (img
+        .unfold(1, h, h)
+        .unfold(2, w, w)
+        .permute(1, 2, 0, 3, 4)
+    )
+    assert isGCHW(ret), ret.shape
+    return ret
+
+def splitHWC(img: ImgHWC, h: int, w: int) -> ImgGHWC:
+    assert img.shape[0] % h == 0
+    assert img.shape[1] % w == 0
+    ret = (img
+        .unfold(0, h, h)
+        .unfold(1, w, w)
+        .permute(0, 1, 3, 4, 2)
+    )
+    assert isGHWC(ret), ret.shape
+    return ret
+
+def reconstructGCHW(img: ImgGCHW) -> ImgCHW:
+    _img = img.permute(2, 0, 3, 1, 4)
+    C, GH, H, GW, W = _img.shape
+    ret = _img.reshape(C, GH * H, GW * W)
+    assert isCHW(ret), ret.shape
+    return ret
+
+def reconstructGHWC(img: ImgGHWC) -> ImgHWC:
+    _img = img.permute(0, 2, 1, 3, 4)
+    GH, H, GW, W, C = _img.shape
+    ret = _img.reshape(GH * H, GW * W, C)
+    assert isHWC(ret), ret.shape
+    return ret
+
+def flatGCHW(img: ImgGCHW) -> ImgNCHW:
+    GH, GW, *DIM = img.shape
+    ret = img.reshape(GH * GW, *DIM)
+    assert isNCHW(ret), ret.shape
+    return ret
+
+def flatGHWC(img: ImgGHWC) -> ImgNHWC:
+    GH, GW, *DIM = img.shape
+    ret = img.reshape(GH * GW, *DIM)
+    assert isNHWC(ret), ret.shape
+    return ret
+
+def foldNCHW(img: ImgNCHW, h: int, w: int) -> ImgGCHW:
+    N, *DIM = img.shape
+    assert N == h * w
+    ret = img.reshape(h, w, *DIM)
+    assert isGCHW(ret), ret.shape
+    return ret
+
+def foldNHWC(img: ImgNHWC, h: int, w: int) -> ImgGHWC:
+    N, *DIM = img.shape
+    assert N == h * w
+    ret = img.reshape(h, w, *DIM)
+    assert isGHWC(ret), ret.shape
+    return ret
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture('videos/jnc00.mp4')
+    ret, frame = cap.read()
+    cap.release()
+    frame = frame[32 * 10:32 * (10 + 17), 64 * 3: 64 * (3 + 19)]
+    # cv2.imwrite('frame.jpg', frame)
+
+    tframe = torch.tensor(frame)
+    assert isHWC(tframe), tframe.shape
+    img_ghwc = splitHWC(tframe, 32, 64)
+    for i in range(17):
+        for j in range(19):
+            # cv2.imwrite(f'frame_{i}_{j}.jpg', img_ghwc[i, j].numpy())
+            assert torch.all(img_ghwc[i, j] == torch.tensor(frame[i * 32:(i + 1) * 32, j * 64:(j + 1) * 64]))
+
+
+    img = torch.tensor([*range(3 * 5 * 7 * 11 * 13)]).reshape(3, 5 * 13, 7 * 11)
+    assert isCHW(img), img.shape
+    img_gchw = splitCHW(img, 13, 11)
+    for i in range(5):
+        for j in range(7):
+            assert torch.all(img_gchw[i, j] == img[:, i * 13:(i + 1) * 13, j * 11:(j + 1) * 11])
+    
+    assert torch.all(reconstructGCHW(img_gchw) == img)
+
+    img_nchw = flatGCHW(img_gchw)
+    for i in range(5):
+        for j in range(7):
+            assert torch.all(img_nchw[(i * 7) + j] == img_gchw[i, j])
+    
+    assert torch.all(foldNCHW(img_nchw, 5, 7) == img_gchw)
+
+
+    img = torch.tensor([*range(3 * 5 * 7 * 11 * 13)]).reshape(5 * 13, 7 * 11, 3)
+    assert isHWC(img), img.shape
+    img_ghwc = splitHWC(img, 13, 11)
+    for i in range(5):
+        for j in range(7):
+            assert torch.all(img_ghwc[i, j] == img[i * 13:(i + 1) * 13, j * 11:(j + 1) * 11])
+    
+    assert torch.all(reconstructGHWC(img_ghwc) == img)
+
+    img_nhwc = flatGHWC(img_ghwc)
+    for i in range(5):
+        for j in range(7):
+            assert torch.all(img_nhwc[(i * 7) + j] == img_ghwc[i, j])
+    
+    assert torch.all(foldNHWC(img_nhwc, 5, 7) == img_ghwc)
+    
--- track.py
+++ track.py
@@ -15,6 +15,7 @@ import torch
 from typing import NamedTuple
 import multiprocessing as mp
 import shutil
+import time
 
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
@@ -61,116 +62,145 @@ def main(args):
     videofilename = args.video.split('/')[-1]
     device = f'cuda:{args.gpu}'
 
-    with open(CONFIG) as fp:
-        config = json.load(fp)
-    cfg = get_cfg()
-    cfg.merge_from_file(os.path.join('./modules/detectron2/configs', config['config']))
-    cfg.MODEL.WEIGHTS = config['weights']
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = config['num_classes']
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = config['score_threshold']
-    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = config['score_threshold']
-    cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = config['nms_threshold']
-    cfg.MODEL.RETINANET.NMS_THRESH_TEST = config['nms_threshold']
-    cfg.TEST.DETECTIONS_PER_IMAGE = config['detections_per_image']
-    cfg.MODEL.ANCHOR_GENERATOR.SIZES = config['anchor_generator_sizes']
-    cfg.MODEL.DEVICE = device
-    predictor = DefaultPredictor(cfg)
-    tree = ElementTree.parse(MASK)
-    mask = tree.getroot()
-    mask = mask.find(f'.//image[@name="{videofilename[:-len('.mp4')]}.jpg"]')
-    assert isinstance(mask, ElementTree.Element)
-
-    tracker = Sort(max_age=5)
-    cap = cv2.VideoCapture(os.path.expanduser(args.video))
-    trajectories = {}
-    # rendering = {}
-    frame_index = 0
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    frame_sep = frame_count // 5
-
-    fpd = open(os.path.join('track-results', f'{videofilename}.d.jsonl'), 'w')
-    fpr = open(os.path.join('track-results', f'{videofilename}.r.jsonl'), 'w')
-
     with torch.no_grad():
-        bmmask, btl, bbr = get_mask(mask, width, height)
-        bmmask = bmmask[btl[0]:bbr[0], btl[1]:bbr[1], :]
-        bmmask = torch.from_numpy(bmmask).to(device)
+        fpd = open(os.path.join('track-results', f'{videofilename}.d.jsonl'), 'w')
+        fpr = open(os.path.join('track-results', f'{videofilename}.r.jsonl'), 'w')
+        fpp = open(os.path.join('track-results', f'{videofilename}.p.jsonl'), 'w')
+        try:
+            with open(CONFIG) as fp:
+                config = json.load(fp)
+            cfg = get_cfg()
+            cfg.merge_from_file(os.path.join('./modules/detectron2/configs', config['config']))
+            cfg.MODEL.WEIGHTS = config['weights']
+            cfg.MODEL.ROI_HEADS.NUM_CLASSES = config['num_classes']
+            cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = config['score_threshold']
+            cfg.MODEL.RETINANET.SCORE_THRESH_TEST = config['score_threshold']
+            cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = config['nms_threshold']
+            cfg.MODEL.RETINANET.NMS_THRESH_TEST = config['nms_threshold']
+            cfg.TEST.DETECTIONS_PER_IMAGE = config['detections_per_image']
+            cfg.MODEL.ANCHOR_GENERATOR.SIZES = config['anchor_generator_sizes']
+            cfg.MODEL.DEVICE = device
+            predictor = DefaultPredictor(cfg)
+            tree = ElementTree.parse(MASK)
+            mask = tree.getroot()
+            mask = mask.find(f'.//image[@name="{videofilename[:-len('.mp4')]}.jpg"]')
+            assert isinstance(mask, ElementTree.Element)
 
-        while cap.isOpened():
-            print('Parsing frame {:d} / {:d}...'.format(frame_index, frame_count))
-            success, frame = cap.read()
-            if not success:
-                break
-            # frame_masked = mask_frame(frame, mask)
-            frame_masked = frame[btl[0]:bbr[0], btl[1]:bbr[1], :]
-            frame_masked = torch.from_numpy(frame_masked).to(device) * bmmask
-            frame_masked = frame_masked.detach().cpu().numpy()
+            tracker = Sort(max_age=5)
+            cap = cv2.VideoCapture(os.path.expanduser(args.video))
+            trajectories = {}
+            frame_index = 0
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-            if frame_index % frame_sep == 0:
-                cv2.imwrite(os.path.join('track-results', f'{videofilename}.{frame_index}.jpg'), frame_masked)
+            frame_sep = frame_count // 5
 
-            image_regions = regionize_image(frame_masked)
-            bboxes = []
-            scores = []
-            for _image, _offset in image_regions:
-                _outputs = predictor(_image)
-                _bboxes, _scores, _ = parse_outputs(_outputs, _offset)
-                bboxes += _bboxes
-                scores += _scores
-            nms_threshold = config['nms_threshold']
-            nms_bboxes, nms_scores = nms(bboxes, scores, nms_threshold)
-            detections = np.zeros((len(nms_bboxes), 5))
-            detections[:, 0:4] = nms_bboxes
-            detections[:, 4] = nms_scores
-            fpd.write(json.dumps([frame_index, detections.tolist()]) + '\n')
+            bmmask, btl, bbr = get_mask(mask, width, height)
+            bmmask = bmmask[btl[0]:bbr[0], btl[1]:bbr[1], :]
+            bmmask = torch.from_numpy(bmmask).to(device)
 
-            tracked_objects = tracker.update(detections)
-            # rendering[frame_index] = []
-            rendering = []
-            for tracked_object in tracked_objects:
-                tl = (int(tracked_object[0]), int(tracked_object[1]))
-                br = (int(tracked_object[2]), int(tracked_object[3]))
-                object_index = int(tracked_object[4])
-                if object_index not in trajectories:
-                    trajectories[object_index] = []
-                trajectories[object_index].append([
-                    frame_index, tl[0], tl[1], br[0], br[1]])
-                rendering.append([
-                    object_index, tl[0], tl[1], br[0], br[1]])
-            fpr.write(json.dumps([frame_index, rendering]) + '\n')
 
-            if frame_index % 50 == 0:
-                fpd.flush()
-                fpr.flush()
-            
-            frame_masked_r = frame_masked.copy()
-            frame_masked_d = frame_masked.copy()
-            if frame_index % frame_sep == 0:
-                for object_index, tlx, tly, brx, bry in rendering:
-                    cv2.rectangle(frame_masked_r, (tlx, tly), (brx, bry), colors[object_index % len(colors)], 2)
-                    cv2.putText(frame_masked_r, str(object_index), (tlx, tly), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[object_index % len(colors)], 2)
-                cv2.imwrite(os.path.join('track-results', f'{videofilename}.{frame_index}.r.jpg'), frame_masked_r)
-                for bbox in nms_bboxes:
-                    tlx, tly, brx, bry = map(int, bbox)
-                    cv2.rectangle(frame_masked_d, (tlx, tly), (brx, bry), (0, 255, 0), 2)
-                cv2.imwrite(os.path.join('track-results', f'{videofilename}.{frame_index}.d.jpg'), frame_masked_d)
+            def log(action: str, start: int, end: int, **kwargs):
+                start //= 1000
+                end //= 1000
+                fpp.write(json.dumps({
+                    'fid': frame_index,
+                    'action': action,
+                    'time': end - start,
+                    'start': start,
+                    'end': end,
+                    **kwargs
+                }) + '\n')
 
-            frame_index = frame_index + 1
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
-        cap.release()
-        cv2.destroyAllWindows()
 
-        # scenario = args.video.replace('videos/', '').replace('.mp4', '')
-        with open(os.path.join('track-results', f'{videofilename}.t.json'), 'w') as fp:
-            json.dump(trajectories, fp)
-        # with open(os.path.join('track-results', f'{videofilename}.r.json'), 'w') as fp:
-        #     json.dump(rendering, fp)
-    
-    fpd.close()
-    fpr.close()
+            while cap.isOpened():
+                print(f'{videofilename} -- GPU {device}' + 'Parsing frame {:d} / {:d}...'.format(frame_index, frame_count))
+                start = time.time_ns()
+                success, frame = cap.read()
+                if not success:
+                    break
+                end = time.time_ns()
+                log('read', start, end)
+
+                start = time.time_ns()
+                # frame_masked = mask_frame(frame, mask)
+                frame_masked = frame[btl[0]:bbr[0], btl[1]:bbr[1], :]
+                frame_masked = torch.from_numpy(frame_masked).to(device) * bmmask
+                frame_masked = frame_masked.detach().cpu().numpy()
+                end = time.time_ns()
+                log('mask', start, end)
+
+                if frame_index % frame_sep == 0:
+                    cv2.imwrite(os.path.join('track-results', f'{videofilename}.{frame_index}.0.jpg'), frame_masked)
+
+                start = time.time_ns()
+                image_regions = regionize_image(frame_masked)
+                bboxes = []
+                scores = []
+                resolutions = []
+                for _image, _offset in image_regions:
+                    resolutions.append(_image.shape)
+                    _outputs = predictor(_image)
+                    _bboxes, _scores, _ = parse_outputs(_outputs, _offset)
+                    bboxes += _bboxes
+                    scores += _scores
+                nms_threshold = config['nms_threshold']
+                nms_bboxes, nms_scores = nms(bboxes, scores, nms_threshold)
+                detections = np.zeros((len(nms_bboxes), 5))
+                detections[:, 0:4] = nms_bboxes
+                detections[:, 4] = nms_scores
+                end = time.time_ns()
+                log('detect', start, end, resolutions=resolutions, num_detections=len(detections), num_regions=len(image_regions))
+
+                fpd.write(json.dumps([frame_index, detections.tolist()]) + '\n')
+
+                start = time.time_ns()
+                tracked_objects = tracker.update(detections)
+                end = time.time_ns()
+                log('track', start, end, num_tracks=len(tracked_objects))
+
+                rendering = []
+                for tracked_object in tracked_objects:
+                    tl = (int(tracked_object[0]), int(tracked_object[1]))
+                    br = (int(tracked_object[2]), int(tracked_object[3]))
+                    object_index = int(tracked_object[4])
+                    if object_index not in trajectories:
+                        trajectories[object_index] = []
+                    trajectories[object_index].append([
+                        frame_index, tl[0], tl[1], br[0], br[1]])
+                    rendering.append([
+                        object_index, tl[0], tl[1], br[0], br[1]])
+                fpr.write(json.dumps([frame_index, rendering]) + '\n')
+
+                if frame_index % 50 == 0:
+                    fpd.flush()
+                    fpr.flush()
+                    fpp.flush()
+                
+                frame_masked_r = frame_masked.copy()
+                frame_masked_d = frame_masked.copy()
+                if frame_index % frame_sep == 0:
+                    for object_index, tlx, tly, brx, bry in rendering:
+                        cv2.rectangle(frame_masked_r, (tlx, tly), (brx, bry), colors[object_index % len(colors)], 2)
+                        cv2.putText(frame_masked_r, str(object_index), (tlx, tly), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[object_index % len(colors)], 2)
+                    cv2.imwrite(os.path.join('track-results', f'{videofilename}.{frame_index}.r.jpg'), frame_masked_r)
+                    for bbox in nms_bboxes:
+                        tlx, tly, brx, bry = map(int, bbox)
+                        cv2.rectangle(frame_masked_d, (tlx, tly), (brx, bry), (0, 255, 0), 2)
+                    cv2.imwrite(os.path.join('track-results', f'{videofilename}.{frame_index}.d.jpg'), frame_masked_d)
+
+                frame_index = frame_index + 1
+            cap.release()
+            cv2.destroyAllWindows()
+
+            with open(os.path.join('track-results', f'{videofilename}.t.json'), 'w') as fp:
+                json.dump(trajectories, fp)
+            with open(os.path.join('track-results', f'{videofilename}.done'), 'w') as fp:
+                fp.write('done')
+        finally:
+            fpd.close()
+            fpr.close()
+            fpp.close()
 
 
 if __name__ == '__main__':
